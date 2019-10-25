bc6aeacc7480e94638f2ac199c08f2f1953e26c1
ccomrade <d.hryzbil@gmail.com>
2019-10-25 23:28:27 +0200

    Build system

---
+++ Code/Launcher/Main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief CryMP Client launcher.
+ */
+
+#include "CryCommon/CryModuleDefs.h"
+#include "CryCommon/platform_impl.h"
+
+#include "CryCommon/platform.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
+
