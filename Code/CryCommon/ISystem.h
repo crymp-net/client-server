565d8d94ed4ba61a8480e03312663138b13c91f2
ccomrade <d.hryzbil@gmail.com>
2020-02-15 19:55:02 +0100

    CryCommon: Add reverse engineered SSystemInitParams

--- Code/CryCommon/ISystem.h
+++ Code/CryCommon/ISystem.h
@@ -367,9 +367,30 @@ struct ISystemEventDispatcher
 };
 
 
+/**
+ * @brief Reverse engineered CryEngine initialization parameters.
+ * Crytek removed content of this structure from the Mod SDK. Total size is 2384 B in 32-bit code and 2464 B in 64-bit code.
+ */
+struct SSystemInitParams
+{
+	void *hInstance;                     // executable handle
+	void *hWnd;                          // optional window handle
+	ILog *pLog;                          // optional custom log
+	ILogCallback *pLogCallback;          // optional log callback
+	ISystemUserCallback *pUserCallback;  // optional engine callback
+	const char *sLogFileName;            // usually "Game.log" or "Server.log"
+	IValidator *pValidator;              // optional custom validator
+	char szSystemCmdLine[2048];          // process command line obtained with GetCommandLineA
+	char szUserPath[256];                // optional custom user folder in %USERPROFILE%\Documents
+	bool bEditor;                        // editor mode
+	bool bMinimal;                       // minimal mode - skip initialization of some subsystems
+	bool bTesting;                       // test mode
+	bool bDedicatedServer;               // launch dedicated server
+	ISystem *pSystem;                    // initialized by IGameStartup::Init
+	void *pCheckFunc;                    // not used
+	void *pProtectedFunctions[10];       // probably not used
+};
 
-// Structure passed to Init method of ISystem interface.
-struct SSystemInitParams{};
 
 // Typedef for frame profile callback function.
 typedef void (*FrameProfilerSectionCallback)( class CFrameProfilerSection *pSection );
