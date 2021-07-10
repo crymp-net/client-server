6b2c93e0ddb40fb71d00cfd36ad5ea7442aa77da
ccomrade <d.hryzbil@gmail.com>
2021-07-10 23:45:29 +0200

    Add some utilities

--- Code/Library/Util.cpp
+++ Code/Library/Util.cpp
@@ -1,16 +1,26 @@
 #include <ctype.h>
+#include <wctype.h>
+#include <string.h>
 #include <array>
-#include <algorithm>
 
 #include "Util.h"
 #include "Format.h"
 #include "StringBuffer.h"
+#include "External/picosha2.h"
 
 bool Util::EqualNoCase(const std::string_view & stringA, const std::string_view & stringB)
 {
 	return std::equal(stringA.begin(), stringA.end(), stringB.begin(), stringB.end(), [](char a, char b) -> bool
 	{
 		return tolower(a) == tolower(b);
+	});
+}
+
+bool Util::EqualNoCase(const std::wstring_view & stringA, const std::wstring_view & stringB)
+{
+	return std::equal(stringA.begin(), stringA.end(), stringB.begin(), stringB.end(), [](wchar_t a, wchar_t b) -> bool
+	{
+		return towlower(a) == towlower(b);
 	});
 }
 
@@ -22,12 +32,57 @@ bool Util::StartsWith(const std::string_view & prefix, const std::string_view & 
 		return false;
 }
 
+bool Util::StartsWith(const std::wstring_view & prefix, const std::wstring_view & text)
+{
+	if (prefix.length() <= text.length())
+		return prefix == std::wstring_view(text.data(), prefix.length());
+	else
+		return false;
+}
+
 bool Util::StartsWithNoCase(const std::string_view & prefix, const std::string_view & text)
 {
 	if (prefix.length() <= text.length())
 		return EqualNoCase(prefix, std::string_view(text.data(), prefix.length()));
 	else
 		return false;
+}
+
+bool Util::StartsWithNoCase(const std::wstring_view & prefix, const std::wstring_view & text)
+{
+	if (prefix.length() <= text.length())
+		return EqualNoCase(prefix, std::wstring_view(text.data(), prefix.length()));
+	else
+		return false;
+}
+
+bool Util::PathStartsWith(const std::filesystem::path & prefix, const std::filesystem::path & path)
+{
+	auto [prefixIt, pathIt] = std::mismatch(
+		prefix.begin(),
+		prefix.end(),
+		path.begin(),
+		path.end(),
+		[](const std::filesystem::path & elementA, const std::filesystem::path & elementB) -> bool
+		{
+			return EqualNoCase(elementA.native(), elementB.native());
+		}
+	);
+
+	return prefixIt == prefix.end();
+}
+
+const char *Util::CopyToBuffer(const std::string_view & text, char *buffer, size_t bufferSize)
+{
+	if (buffer && bufferSize > 0)
+	{
+		const size_t length = std::min(text.length(), bufferSize - 1);
+
+		memcpy(buffer, text.data(), length);
+		buffer[length] = '\0';
+	}
+
+	return buffer;
 }
 
 std::string Util::MakeHumanReadableBytes(uint64_t bytes)
@@ -92,4 +147,20 @@ std::string Util::SecondsToString(uint64_t seconds)
 	buffer.addUInt(seconds, 2);
 
 	return buffer.toString();
+}
+
+std::string Util::sha256(const std::string_view & text)
+{
+	// lowercase hex digits
+	return picosha2::hash256_hex_string(text);
+}
+
+std::string Util::SHA256(const std::string_view & text)
+{
+	std::string result = sha256(text);
+
+	// uppercase hex digits
+	std::transform(result.begin(), result.end(), result.begin(), toupper);
+
+	return result;
 }
--- Code/Library/Util.h
+++ Code/Library/Util.h
@@ -4,15 +4,46 @@
 #include <stdint.h>
 #include <string>
 #include <string_view>
+#include <filesystem>
+#include <algorithm>
 
 namespace Util
 {
 	bool EqualNoCase(const std::string_view & stringA, const std::string_view & stringB);
+	bool EqualNoCase(const std::wstring_view & stringA, const std::wstring_view & stringB);
 
 	bool StartsWith(const std::string_view & prefix, const std::string_view & text);
+	bool StartsWith(const std::wstring_view & prefix, const std::wstring_view & text);
 	bool StartsWithNoCase(const std::string_view & prefix, const std::string_view & text);
+	bool StartsWithNoCase(const std::wstring_view & prefix, const std::wstring_view & text);
+
+	bool PathStartsWith(const std::filesystem::path & prefix, const std::filesystem::path & path);
+
+	const char *CopyToBuffer(const std::string_view & text, char *buffer, size_t bufferSize);
 
 	std::string MakeHumanReadableBytes(uint64_t bytes);
 
 	std::string SecondsToString(uint64_t seconds);
+
+	std::string sha256(const std::string_view & text);  // lowercase hex digits
+	std::string SHA256(const std::string_view & text);  // uppercase hex digits
+
+	inline std::string_view RemovePrefix(std::string_view text, size_t length)
+	{
+		text.remove_prefix(std::min(length, text.length()));
+		return text;
+	}
+
+	inline std::string_view RemoveSuffix(std::string_view text, size_t length)
+	{
+		text.remove_suffix(std::min(length, text.length()));
+		return text;
+	}
+
+	inline std::string_view RemovePrefixAndSuffix(std::string_view text, size_t prefixLength, size_t suffixLength)
+	{
+		text.remove_prefix(std::min(prefixLength, text.length()));
+		text.remove_suffix(std::min(suffixLength, text.length()));
+		return text;
+	}
 }
