ffca091885a6e168177d0f6e0ac25fbb93e12f85
griseraner <griseraner@gmail.com>
2022-12-07 13:16:48 +0100

    update Player.h

--- Code/CryGame/Actors/Player/Player.h
+++ Code/CryGame/Actors/Player/Player.h
@@ -554,7 +554,6 @@ public:
 	virtual EntityId GetDeathCamTarget() const { return m_stats.deathCamTarget; };
 	virtual void SetSpectatorHealth(int health) { m_stats.spectatorHealth = health; };
 	virtual int GetSpectatorHealth() const { return m_stats.spectatorHealth; };
-	virtual void ChangeSpectatorZoom(int zoomChange) { m_stats.spectatorZoom = CLAMP(m_stats.spectatorZoom + zoomChange, 2, 6); }
 	virtual void ChangeSpectatorZoom(int zoomChange) { m_stats.spectatorZoom = CLAMP(m_stats.spectatorZoom + zoomChange, 2, 8); }
 	virtual int GetSpectatorZoom() const { return m_stats.spectatorZoom; }
 	void MoveToSpectatorTargetPosition();
