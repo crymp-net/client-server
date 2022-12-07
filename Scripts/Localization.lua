ff1de31e4fd382a4d6ad8c91d21c25465036a4c1
ccomrade <d.hryzbil@gmail.com>
2022-12-07 21:20:55 +0100

    #66: Fix Localization.lua

--- Scripts/Localization.lua
+++ Scripts/Localization.lua
@@ -13,7 +13,7 @@ CPPAPI.AddLocalizedLabel("mp_american/us_commander_mission_2_minute_warning_01",
 	use_subtitle = false,
 	languages = {
 		czech = {
-			translated_text = "2 minuty do ukončení mise.",
+			localized_text = "2 minuty do ukončení mise.",
 		},
 		english = {
 			sound_volume = 0.4,
@@ -22,47 +22,47 @@ CPPAPI.AddLocalizedLabel("mp_american/us_commander_mission_2_minute_warning_01",
 			sound_radio_squelch = 0.6,
 		},
 		french = {
-			translated_text = "2 minutes avant la fin de la mission.",
+			localized_text = "2 minutes avant la fin de la mission.",
 		},
 		german = {
-			translated_text = "2 Minuten bis zum Abbruch der Mission.",
+			localized_text = "2 Minuten bis zum Abbruch der Mission.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		hungarian = {
-			translated_text = "Az akció lefújásáig 2 perc van hátra.",
+			localized_text = "Az akció lefújásáig 2 perc van hátra.",
 		},
 		italian = {
-			translated_text = "2 minuti al termine della missione.",
+			localized_text = "2 minuti al termine della missione.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		japanese = {
-			translated_text = "ミッション終了まで、あと2分",
+			localized_text = "ミッション終了まで、あと2分",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.8,
 			sound_radio_squelch = 0.6,
 		},
 		polish = {
-			translated_text = "Dwie minuty do przerwania misji.",
+			localized_text = "Dwie minuty do przerwania misji.",
 		},
 		russian = {
-			translated_text = "2 минуты до окончания операции.",
+			localized_text = "2 минуты до окончания операции.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		spanish = {
-			translated_text = "2 minutos para la finalización de la misión.",
+			localized_text = "2 minutos para la finalización de la misión.",
 		},
 		turkish = {
-			translated_text = "Görev bitimine 2 dakika kaldı.",
+			localized_text = "Görev bitimine 2 dakika kaldı.",
 		},
 	},
 })
@@ -78,7 +78,7 @@ CPPAPI.AddLocalizedLabel("mp_american/us_commander_mission_1_minute_warning_02",
 	use_subtitle = false,
 	languages = {
 		czech = {
-			translated_text = "1 minuta do ukončení mise.",
+			localized_text = "1 minuta do ukončení mise.",
 		},
 		english = {
 			sound_volume = 0.4,
@@ -87,47 +87,47 @@ CPPAPI.AddLocalizedLabel("mp_american/us_commander_mission_1_minute_warning_02",
 			sound_radio_squelch = 0.6,
 		},
 		french = {
-			translated_text = "1 minute avant la fin de la mission.",
+			localized_text = "1 minute avant la fin de la mission.",
 		},
 		german = {
-			translated_text = "1 Minute bis zum Abbruch der Mission.",
+			localized_text = "1 Minute bis zum Abbruch der Mission.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		hungarian = {
-			translated_text = "Az akció lefújásáig 1 perc van hátra.",
+			localized_text = "Az akció lefújásáig 1 perc van hátra.",
 		},
 		italian = {
-			translated_text = "1 minuto al termine della missione.",
+			localized_text = "1 minuto al termine della missione.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		japanese = {
-			translated_text = "ミッション終了まで、あと1分",
+			localized_text = "ミッション終了まで、あと1分",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.8,
 			sound_radio_squelch = 0.6,
 		},
 		polish = {
-			translated_text = "Jedna minuta do przerwania misji.",
+			localized_text = "Jedna minuta do przerwania misji.",
 		},
 		russian = {
-			translated_text = "Минута до окончания операции.",
+			localized_text = "Минута до окончания операции.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		spanish = {
-			translated_text = "1 minuto para la finalización de la misión.",
+			localized_text = "1 minuto para la finalización de la misión.",
 		},
 		turkish = {
-			translated_text = "Görev bitimine 1 dakika kaldı.",
+			localized_text = "Görev bitimine 1 dakika kaldı.",
 		},
 	},
 })
@@ -143,7 +143,7 @@ CPPAPI.AddLocalizedLabel("mp_american/us_commander_mission_30_second_03", {
 	use_subtitle = false,
 	languages = {
 		czech = {
-			translated_text = "30 vteřin do ukončení mise.",
+			localized_text = "30 vteřin do ukončení mise.",
 		},
 		english = {
 			sound_volume = 0.4,
@@ -152,54 +152,54 @@ CPPAPI.AddLocalizedLabel("mp_american/us_commander_mission_30_second_03", {
 			sound_radio_squelch = 0.6,
 		},
 		french = {
-			translated_text = "30 secondes avant la fin de la mission.",
+			localized_text = "30 secondes avant la fin de la mission.",
 		},
 		german = {
-			translated_text = "30 Sekunden bis zum Abbruch der Mission.",
+			localized_text = "30 Sekunden bis zum Abbruch der Mission.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		hungarian = {
-			translated_text = "Az akció lefújásáig 30 másodperc van hátra.",
+			localized_text = "Az akció lefújásáig 30 másodperc van hátra.",
 		},
 		italian = {
-			translated_text = "30 secondi al termine della missione.",
+			localized_text = "30 secondi al termine della missione.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		japanese = {
-			translated_text = "ミッション終了まで、あと30秒",
+			localized_text = "ミッション終了まで、あと30秒",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.8,
 			sound_radio_squelch = 0.6,
 		},
 		polish = {
-			translated_text = "Trzydzieści sekund do przerwania misji.",
+			localized_text = "Trzydzieści sekund do przerwania misji.",
 		},
 		russian = {
-			translated_text = "30 секунд.",
+			localized_text = "30 секунд.",
 			sound_volume = 0.4,
 			sound_radio_ratio = 0.0,
 			sound_radio_background = 0.2,
 			sound_radio_squelch = 0.6,
 		},
 		spanish = {
-			translated_text = "30 segundos para la finalización de la misión.",
+			localized_text = "30 segundos para la finalización de la misión.",
 		},
 		turkish = {
-			translated_text = "Görev bitimine 30 saniye kaldı.",
+			localized_text = "Görev bitimine 30 saniye kaldı.",
 		},
 	},
 })
 
 CPPAPI.AddLocalizedLabel("mp_american/us_commander_endgame_countdown_01", {
 	english_text = "5, 4, 3, 2, 1",
-	translated_text = "5, 4, 3, 2, 1",
+	localized_text = "5, 4, 3, 2, 1",
 	sound_volume = 0.6,
 	sound_event = "radio2d",
 	sound_radio_ratio = 0.05,
@@ -249,34 +249,34 @@ CPPAPI.AddLocalizedLabel("Detonator", {
 	english_text = "DETONATOR",
 	languages = {
 		czech = {
-			translated_text = "DETONÁTOR",
+			localized_text = "DETONÁTOR",
 		},
 		french = {
-			translated_text = "DÉTONATEUR",
+			localized_text = "DÉTONATEUR",
 		},
 		german = {
-			translated_text = "AUSLÖSER",
+			localized_text = "AUSLÖSER",
 		},
 		hungarian = {
-			translated_text = "DETONÁTOR",
+			localized_text = "DETONÁTOR",
 		},
 		italian = {
-			translated_text = "DETONATORE",
+			localized_text = "DETONATORE",
 		},
 		japanese = {
-			translated_text = "雷管",
+			localized_text = "雷管",
 		},
 		polish = {
-			translated_text = "DETONATOR",
+			localized_text = "DETONATOR",
 		},
 		russian = {
-			translated_text = "ДЕТОНАТОР",
+			localized_text = "ДЕТОНАТОР",
 		},
 		spanish = {
-			translated_text = "DETONADOR",
+			localized_text = "DETONADOR",
 		},
 		turkish = {
-			translated_text = "DETONATÖR",
+			localized_text = "DETONATÖR",
 		},
 	},
 })
