ef264366ec8e296ddc5156658db9642d1bdd6208
ccomrade <d.hryzbil@gmail.com>
2024-11-14 22:19:33 +0100

    Fix indentation in Resources.h

--- Code/Launcher/Resources.h
+++ Code/Launcher/Resources.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define RESOURCE_ICON                        101
+#define RESOURCE_ICON                     101
 
-#define RESOURCE_CURSOR_AMBER                103
-#define RESOURCE_CURSOR_BLUE                 104
-#define RESOURCE_CURSOR_GREEN                105
-#define RESOURCE_CURSOR_RED                  106
-#define RESOURCE_CURSOR_WHITE                107
+#define RESOURCE_CURSOR_AMBER             103
+#define RESOURCE_CURSOR_BLUE              104
+#define RESOURCE_CURSOR_GREEN             105
+#define RESOURCE_CURSOR_RED               106
+#define RESOURCE_CURSOR_WHITE             107
 
 #define RESOURCE_MASTERS_TXT              110
 
